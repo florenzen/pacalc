ec2e8cd3b9ec0b1bc87aee75dbbbbe0cff715a9a
Florian Lorenzen <florian.lorenzen@posteo.de>
2025-04-20 11:19:57 +0000

    Move colors into its own module

---
+++ src/colors.rs
@@ -0,0 +1,9 @@
+#![allow(dead_code)]
+
+pub const BLUE1: &str = "#1568a8";
+pub const BLUE2: &str = "#2a4050";
+pub const BLUE3: &str = "#214c9a";
+pub const BLUE4: &str = "#1999e8";
+pub const BLUE5: &str = "#1b242c";
+pub const GREY: &str = "#ced2d6";
+pub const WHITE: &str = "#ffffff";
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,9 @@ use leptos::*;
 use std::collections::HashMap;
 use std::time::Duration;
 
-pub const TITLE: &str = "Pace calculator";
+mod colors;
 
-mod colors {
-    pub const BLUE1: &str = "#1568a8";
-    pub const BLUE2: &str = "#2a4050";
-    pub const BLUE3: &str = "#214c9a";
-    pub const BLUE4: &str = "#1999e8";
-    pub const BLUE5: &str = "#1b242c";
-    pub const GREY: &str = "#ced2d6";
-    pub const WHITE: &str = "#ffffff";
-}
+const TITLE: &str = "Pace Calculator";
 
 #[derive(Clone, Debug, PartialEq)]
 struct FormState {
