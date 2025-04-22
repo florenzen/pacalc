8a37c77ad5461ecb6c27e1fefadcadd12a83d795
Florian Lorenzen <florian.lorenzen@posteo.de>
2025-04-22 22:17:23 +0000

    Make tailwind usable by trunk

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,3 +4,10 @@ cargo install trunk
 cargo install leptosfmt
 rustup target add wasm32-unknown-unknown
 npm install
+
+# Trunk expects a tailwindcss executable in the PATH
+cat << EOF  > /home/vscode/.local/bin/tailwindcss
+#!/bin/bash
+npx @tailwindcss/cli $@
+EOF
+chmod +x /home/vscode/.local/bin/tailwindcss
