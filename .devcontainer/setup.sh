900c3e353b0800fb38ba81890bf66030105ceb91
Florian Lorenzen <florian.lorenzen@posteo.de>
2025-04-22 22:34:28 +0000

    Speed up DevContainer build

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-cargo install trunk
-cargo install leptosfmt
-rustup target add wasm32-unknown-unknown
 npm install
 
 # Trunk expects a tailwindcss executable in the PATH
-cat << EOF  > /home/vscode/.local/bin/tailwindcss
+mkdir -p /home/vscode/.local/bin
+cat << 'EOF'  > /home/vscode/.local/bin/tailwindcss
 #!/bin/bash
 npx @tailwindcss/cli $@
 EOF
 chmod +x /home/vscode/.local/bin/tailwindcss
+
+# Since some cargo installs are done in the Dockerfile
+# some ownerships are root which prevents trunk, e. g.,
+# from installing dependencies.
+sudo chown -R vscode /usr/local/cargo
